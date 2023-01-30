d7e4fe8da7960cf90aca01b812f131a99d8a5513
appleseed619 <sungan_555_3@outlook.com>
2023-01-30 20:33:22 +0300

    order recording system added

--- src/app/shop/checkout/checkout.component.ts
+++ src/app/shop/checkout/checkout.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Order } from 'src/app/model/order.model';
+import { OrderRepository } from 'src/app/model/order.repository';
+
 
 @Component({
   selector: 'checkout',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent {
+  orderSent: boolean = false;
+  submitted: boolean = false;
+
+  constructor(public order: Order, private orderRepository: OrderRepository) { }
+
+  submitOrder(form: NgForm) {
+    this.submitted = true;
+    if (form.valid) {
+      this.orderRepository.saveOrder(this.order)
+        .subscribe(order => {
+          this.order.clearOrder();
+          this.orderSent = true;
+          this.submitted = false;
+        });
+    }
+  }
 
 }
