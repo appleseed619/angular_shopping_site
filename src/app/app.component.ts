1bf7f22fa852f0bd0fd25f27db0c2b1c42eae502
appleseed619 <sungan_555_3@outlook.com>
2023-01-30 15:00:17 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'root',
+  template: `<div class="bg-primary m-5 p-5">Shop Application</div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-shop-app';
 }
