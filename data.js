8b35242e496dab4668ec574dd6291df712f7c037
appleseed619 <sungan_555_3@outlook.com>
2023-01-30 15:10:01 +0300

    json-server added

---
+++ data.js
@@ -0,0 +1,19 @@
+module.exports = function(){
+    return {
+        products: [
+            {id: 1, name:"Samsung S5", price: 1000, imageUrl:'1.jpg', description: 'Good phone'},
+            {id: 2, name:"Samsung S6", price: 2000, imageUrl:'1.jpg', description: 'Good phone'},
+            {id: 3, name:"Samsung S7", price: 3000, imageUrl:'1.jpg', description: 'Good phone'},
+            {id: 4, name:"Samsung S8", price: 4000, imageUrl:'1.jpg', description: 'Good phone'},
+            {id: 5, name:"Samsung S9", price: 5000, imageUrl:'1.jpg', description: 'Good phone'},
+            {id: 6, name:"Samsung S10", price: 6000, imageUrl:'1.jpg', description: 'Good phone'},
+            {id: 7, name:"Samsung S11", price: 7000, imageUrl:'1.jpg', description: 'Good phone'},
+        ],
+        categories: [
+            {id: 1, name: 'Phone'},
+            {id: 2, name: 'Computer'},
+            {id: 3, name: 'Electronics'},
+        ],
+        orders: []
+    }
+}
