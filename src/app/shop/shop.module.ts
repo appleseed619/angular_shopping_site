8af037e9dce577dc802ae1880f518aaae1853ca2
appleseed619 <sungan_555_3@outlook.com>
2023-01-30 16:06:03 +0300

    navbar component added

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -3,10 +3,11 @@ import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { ModelModule } from "../model/model.module";
 import { ShopComponent } from "./shop.component";
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [ShopComponent],
+    declarations: [ShopComponent, NavbarComponent],
     exports: [ShopComponent],
 })
 export class ShopModule { }
