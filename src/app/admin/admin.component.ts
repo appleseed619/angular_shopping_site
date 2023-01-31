fd161dd7a8cc22a4e8011d7b7add80d7d2f9ab0f
appleseed619 <sungan_555_3@outlook.com>
2023-01-31 13:24:12 +0300

    logout added

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,10 +1,16 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../model/auth.service";
 
 
 @Component({
     templateUrl: 'admin.component.html'
 })
 export class AdminComponent {
-    constructor() {
+    constructor(private authService: AuthService, private router: Router) { }
+
+    logout() {
+        this.authService.clear();
+        this.router.navigateByUrl("/shop");
     }
 }
