4357ce8861a5fa9605097c014fc6b2d4955140bf
appleseed619 <sungan_555_3@outlook.com>
2023-01-31 13:15:10 +0300

    auth guard added

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { AuthGuard } from './auth.guard';
 import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   { path: 'auth', component: AuthComponent },
-  { path: 'main', component: AdminComponent },
+  { path: 'main', component: AdminComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'auth' }
 ];
 
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,6 +5,7 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AuthComponent } from './auth/auth.component';
 import { AdminComponent } from './admin.component';
 import { FormsModule } from '@angular/forms';
+import { AuthGuard } from './auth.guard';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     FormsModule,
     AdminRoutingModule,
-  ]
+  ],
+  providers: [AuthGuard]
 })
 export class AdminModule { }
